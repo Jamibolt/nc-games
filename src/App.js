3edb90dd535d7f3a2bd85df3c7ad57edcf9ca0bb
Jamie Radcliffe <jamie_michael@hotmail.co.uk>
2023-03-15 15:39:12 +0000

    Added single review parametric endpoint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Header from "../src/components/Header";
 import ReviewList from "../src/components/ReviewList";
+import SingleReview from "../src/components/SingleReview";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ReviewList />
+      <Routes>
+        <Route path="/" element={<ReviewList />} />
+        <Route path="/reviews/:review_id" element={<SingleReview />} />
+      </Routes>
     </div>
   );
 }
